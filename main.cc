db7b4a1caad187a752f3007fe2c426c351220cab
Эльдар Миннахметов <ciborg.brain@yandex.ru>
2022-07-13 03:59:59 +0300

    Add index.html and bash script

--- main.cc
+++ main.cc
@@ -1,7 +1,8 @@
 #include <drogon/drogon.h>
+
 int main() {
     //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0",80);
+    drogon::app().addListener("127.0.0.1", 8081);
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
     //Run HTTP framework,the method will block in the internal event loop
